075952de59a5e0d9ea8066c1bc3b872e9b1b5e77
Vladislav Moruga <Monctpik123@gmail.com>
2023-06-28 14:08:03 +0300

    add function next prev

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,27 @@
 'use strict'
 
-function btnHandler() {
-    console.log('OK')
+const imagesDB = ['https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-border-collie-play-mn-1100-82d2f1.jpg',
+    'https://www.thesprucepets.com/thmb/hxWjs7evF2hP1Fb1c1HAvRi_Rw0=/2765x0/filters:no_upscale():strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg',
+    'https://idsb.tmgrup.com.tr/ly/uploads/images/2021/09/08/142774.jpg'
+];
+
+const [prevBtn, nextBtn] = document.querySelectorAll('.slider-container button')
+const img = document.querySelector('.slider-container .slide img')
+
+const slider = new Slider(imagesDB)
+
+const createSlideBtnHandler = (direction = 'next') => () => {
+    slider.currentIndex = slider[direction === 'next' ? 'next' : 'prev']
+    updateView()
 }
 
+nextBtn.addEventListener('click', createSlideBtnHandler('next'))
+prevBtn.addEventListener('click', createSlideBtnHandler('prev'))
 
-// const [btn] = document.getElementsByTagName('button')
-const btn = document.querySelector('.btn+.btn')
 
-btn.addEventListener('click', btnHandler)
+function updateView(){
+    img.setAttribute('src', slider.currentSlide)
+}
+updateView()
+
+
