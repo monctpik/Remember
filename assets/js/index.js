de8f4913b986ef919a2fa549e84b949aaae2327a
Vladislav Moruga <Monctpik123@gmail.com>
2023-07-01 17:07:46 +0300

    HTML card js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,27 @@
 'use strict'
 
 
+const cardsContainer = document.getElementById('cards-container')
+
+const HTMLCollectionActors = actors.map((actor) => createActorsCard(actor))
+
+function createActorsCard(actor){
+    const HTMLCard = doc
+    return HTMLCard
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -29,7 +50,7 @@
 // }
 //  for (const btn1 of btns1) {
 //   btn1.addEventListener('click', swapBtn);
-}
+// }
 // const imagesDB = ['https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-border-collie-play-mn-1100-82d2f1.jpg',
 //     'https://www.thesprucepets.com/thmb/hxWjs7evF2hP1Fb1c1HAvRi_Rw0=/2765x0/filters:no_upscale():strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg',
 //     'https://idsb.tmgrup.com.tr/ly/uploads/images/2021/09/08/142774.jpg'
